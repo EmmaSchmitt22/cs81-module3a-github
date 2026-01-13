ffc131a853f88f073f537479a317083a08f91468
Emma Schmitt <emmaschmitt22@gmail.com>
2026-01-13 13:51:21 -0800

    Add fetchAge() and calcAge() to journal

--- journal.js
+++ journal.js
@@ -2,3 +2,14 @@
 function greet(name) {
     return "Hello, " + name + "!";
 }
+
+// Ask a user's age
+function fetchAge(age) {
+    return "You are " + age + "years old!";
+}
+
+// Calculate age in 20 years
+function calcAge(age){
+    aged = age + 20
+    return "In 20 years, you will be " + aged + "years old";
+}
